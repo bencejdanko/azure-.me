25bae9a5e2797b1ac7e9d01435977c242818559f
Bence Danko <bencejdanko@gmail.com>
2023-02-20 00:06:12 -0800

    Added collapsible. Added navbar.

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)
+from flask_talisman import Talisman
 
-@app.before_request
-def before_request():
-    if not request.is_secure:
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
+app = Flask(__name__)
+Talisman(app)
 
 @app.route('/')
 def index():
    print('Request for index page received')
    return render_template('index.html')
-
-   
 
 if __name__ == '__main__':
    app.run()
