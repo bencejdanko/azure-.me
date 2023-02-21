cca6450b28971d4b914afd408bdb8a40b165846b
Bence Danko <bencejdanko@gmail.com>
2023-02-20 16:53:11 -0800

    fixing up the css and html files

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect
 from flask_talisman import Talisman
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 Talisman(app)
 
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   return render_template('/templates/index.html')
+
+@app.route('/blog')
+def blog():
+   print('Request for blog page received')
+   return render_template('/templates/blog.html')
 
 if __name__ == '__main__':
    app.run()
