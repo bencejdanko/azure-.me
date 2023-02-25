d29947c4eb71f9b72b05d0e4eb2460e6724bea53
Bence Danko <bencejdanko@gmail.com>
2023-02-24 23:20:37 -0800

    Render blog posts w/ Jinja2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect
 from flask_talisman import Talisman
+from os import listdir as ls
 
 app = Flask(__name__)
 Talisman(app)
@@ -8,11 +9,13 @@ Talisman(app)
 @app.route('/')
 @app.route('/index')
 def index():
+
    return render_template('index.html')
 
 @app.route('/blog')
 def blog():
-   return render_template('blog.html')
+   posts = ls('templates/posts')
+   return render_template('blog.html', posts=posts)
 
 if __name__ == '__main__':
    app.run('0.0.0.0', debug=True, port=8100, ssl_context=('adhoc'))
